320572f0e671325049b1d7e77311eb7197bd3e29
Curlesh <iluminat147741@gmail.com>
2023-05-31 23:55:37 +0400

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿
+namespace Alina_work10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(13, 22);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(311, 24);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 166);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(312, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "1.Найти самое длинное слово",
+            "2.Найти все саммые длинные слова",
+            "3.Найяти самое короткое слово",
+            "4.Найти все самые короткие слова",
+            "5. Слова полиндромы",
+            "6. Вывести слова в алфавитном порядке",
+            "7. В порядке их длинны",
+            "8. Вывести слова которые начинаются на введеную букву"});
+            this.listBox1.Location = new System.Drawing.Point(12, 52);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(312, 108);
+            this.listBox1.TabIndex = 3;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(8, 233);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(79, 20);
+            this.textBox3.TabIndex = 4;
+            this.textBox3.Visible = false;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(157, 18);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Текст для изменений";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(9, 208);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(141, 18);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Буква для 8 пункта";
+            this.label2.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(424, 325);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Alina_work10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string вывод()
+        {
+            using (StreamWriter file = new StreamWriter("file2.txt"))
+            {
+                file.WriteLine(textBox2.Text);
+            }
+            return "";
+        }
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+            int Index = listBox1.SelectedIndex + 1;
+            // label3.Text = Convert.ToString(Index);
+            string line = textBox1.Text;
+            string[] str_arr = line.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            switch (Index)
+            {
+                case 1:
+                    int max = 0,number=0;
+                    for (int i = 0; i < str_arr.Length; i++)
+                    {
+                        if (str_arr[i].Length>max)
+                        {
+                            max = str_arr[i].Length;
+                            number = i;
+                        }
+                    }
+                    textBox2.Text = "";
+                    textBox2.Text = str_arr[number];
+                    вывод();
+                    break;
+                 
+                case 2:
+                    textBox2.Text = "";
+                    max = 0;
+                    number = 0;
+                    for (int i = 0; i < str_arr.Length; i++)
+                    {
+                        if (str_arr[i].Length >= max)
+                        {
+                            max = str_arr[i].Length;
+                            number = i;
+                           
+                        }
+                    }
+                    for (int i = 0; i < str_arr.Length; i++)
+                    {
+                        if (str_arr[i].Length == max)
+                        {
+                            textBox2.Text += str_arr[i] + " ";
+                        }
+                    }
+                    вывод();
+                    break;
+                case 3:
+                    textBox2.Text = "";
+                    int min = 65000;
+                    number = 0;
+                    for (int i = 0; i < str_arr.Length; i++)
+                    {
+                        if (str_arr[i].Length < min)
+                        {
+                            min = str_arr[i].Length;
+                            number = i;
+                          
+                        }
+                    }
+                    textBox2.Text += str_arr[number] + " ";
+                    вывод();
+                    break;
+                case 4:
+                    textBox2.Text = "";
+                    min = 65000;
+                    number = 0;
+                    for (int i = 0; i < str_arr.Length; i++)
+                    {
+                        if (str_arr[i].Length < min)
+                        {
+                            min = str_arr[i].Length;
+                            number = i;
+                            
+                        }
+                    }
+                    for (int i = 0; i <str_arr.Length; i++)
+                    {
+                        if (str_arr[i].Length==min)
+                        {
+                            textBox2.Text += str_arr[i] + " ";
+                        }
+                    }
+                    вывод();
+                    break;
+                case 5:
+                    
+                    textBox2.Text = "";
+                    for (int i = 0; i < str_arr.Length; i++)
+                    {
+                        for (int j = 0; j < str_arr[i].Length; j++)
+                        {
+                            bool flag5 = false;
+                            string word = str_arr[i];
+                            for ( j = 0; j < word.Length; j++)
+                            {
+                                if (word[j]==word[word.Length-1-j])
+                                {
+                                    flag5 = true;
+                                }
+                            }
+                            if (flag5)
+                            {
+                                textBox2.Text += word + " ";
+                            }
+                            
+                        }
+                    }
+                    вывод();
+                    break;
+
+                case 6:
+                    textBox2.Text = "";
+                    Array.Sort(str_arr);
+                    for (int i = 0; i < str_arr.Length; i++)
+                    {
+                        textBox2.Text += str_arr[i]+" ";
+                    }
+                    вывод();
+                    break;
+                case 7:
+                    textBox2.Text = "";
+                    int schet = 0;
+                    for (int i = 0; i < str_arr.Length; i++)
+                    {
+                        schet++;
+                    }
+                    int[] mas = new int[schet];
+                    for (int i = 0; i < str_arr.Length; i++)
+                    {
+                        mas[i] = str_arr[i].Length;
+                    }
+                    Array.Sort(mas, str_arr);
+                    for (int i = 0; i < str_arr.Length; i++)
+                    {
+                        textBox2.Text += str_arr[i]+" ";
+                    }
+                    вывод();
+                    break;
+                case 8:
+                    textBox2.Text = "";
+
+                    textBox3.Visible = true;
+                    label2.Visible = true;
+                    if (textBox3.Text!="")
+                    {
+                        
+                        string l = textBox3.Text;
+                        for (int i = 0; i < str_arr.Length; i++)
+                        {
+
+                            if (str_arr[i].IndexOf(l) == 0)
+                            {
+                                textBox2.Text += str_arr[i] + " ";
+                            }
+                        }
+                        вывод();
+                    }
+                   
+                    break;
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            using (StreamReader file = new StreamReader("file.txt"))
+            {
+                textBox1.Text = file.ReadToEnd();
+                file.Close();
+            }
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            string line = textBox1.Text;
+            string[] str_arr = line.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            textBox2.Text = "";
+
+            if (textBox3.Text != "")
+            {
+                textBox2.Text = "";
+
+                textBox3.Visible = true;
+                label2.Visible = true;
+                string l = textBox3.Text;
+                for (int i = 0; i < str_arr.Length; i++)
+                {
+
+                    if (str_arr[i].IndexOf(l) == 0)
+                    {
+                        textBox2.Text += str_arr[i] + " ";
+                    }
+                }
+                вывод();
+            }
+
+        }
+    }
+}
